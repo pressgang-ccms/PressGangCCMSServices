ac8bc8617370d72f9834947448d1a5b4467b2615
lnewson <lnewson@redhat.com>
2012-06-20 10:58:58 +1000

    Added the shell script to run the builder for translations.

---
+++ docbook-builder-component/run-translations.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+STOMP_SERVER=localhost
+REST_SERVER=http://localhost:8080/TopicIndex/seam/resource/rest
+
+#STOMP_SERVER=skynet.cloud.lab.eng.bne.redhat.com
+#REST_SERVER=http://skynet.cloud.lab.eng.bne.redhat.com:8080/TopicIndex/seam/resource/rest
+PORT=61613
+USER=guest
+PASS=guest
+QUEUE=jms.queue.SkynetDocbookTranslationBuildQueue
+SMTP=smtp.corp.redhat.com
+VERBATIM_XML_ELEMENTS=screen,programlisting
+INLINE_XML_ELEMENTS=code,prompt,command,firstterm,ulink,guilabel,filename,replaceable,parameter,literal,classname,sgmltag,guibutton,guimenuitem,guimenu,menuchoice,citetitle,systemitem,application,acronym,keycap
+CONTENTS_INLINE_XML_ELEMENTS=title,term
+NUMBER_OF_THREADS=1
+MAINCLASS=com.redhat.topicindex.component.docbookrenderer.Main
+
+java -Xmx1024m \
+-Dmail.smtp.host=${SMTP} \
+-DtopicIndex.stompMessageServer=${STOMP_SERVER} \
+-DtopicIndex.stompMessageServerPort=${PORT} \
+-DtopicIndex.stompMessageServerUser=${USER} \
+-DtopicIndex.stompMessageServerPass=${PASS} \
+-DtopicIndex.stompMessageServerQueue=${QUEUE} \
+-DtopicIndex.skynetServer=${REST_SERVER} \
+-DtopicIndex.verbatimXMLElements=${VERBATIM_XML_ELEMENTS} \
+-DtopicIndex.inlineXMLElements=${INLINE_XML_ELEMENTS} \
+-DtopicIndex.contentsInlineXMLElements=${CONTENTS_INLINE_XML_ELEMENTS} \
+-DNumberOfWorkerThreads=${NUMBER_OF_THREADS} \
+-cp target/classes:target/lib/* ${MAINCLASS}
